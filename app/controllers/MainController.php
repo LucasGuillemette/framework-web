f2de904a864f7480710e878542cc7dab6b3867aa
unknown <lucas.gu7777@gmail.com>
2021-03-23 00:06:43 +0100

    ajout store

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -1,8 +1,13 @@
 <?php
 namespace controllers;
+use models\Order;
+use models\Product;
+use models\Section;
 use Ubiquity\attributes\items\router\Route;
 use Ubiquity\controllers\auth\AuthController;
 use Ubiquity\controllers\auth\WithAuthTrait;
+use Ubiquity\orm\DAO;
+use Ubiquity\utils\http\USession;
 
 /**
  * Controller MainController
@@ -19,5 +24,11 @@ class MainController extends ControllerBase{
     protected function getAuthController(): AuthController
     {
         return new MyAuth($this);
+    }
+
+    #[Route ('store', name:'store')]
+    public function store(){
+        $store = DAO::getAll(Product::class, false, false);
+        $this->loadDefaultView(['store'=>$store]);
     }
 }
---
+++ app/services/ui/UiStoreService.php
@@ -0,0 +1,17 @@
+<?php
+namespace services\ui;
+
+use Ajax\php\ubiquity\UIService;
+use Ubiquity\controllers\Controller;
+use Ubiquity\utils\http\URequest;
+
+class UiStoreService extends UIService {
+    public function __construct(Controller $controller) {
+        parent::__construct($controller);
+        if(!URequest::isAjax()) {
+            $this->jquery->getHref('a[data-target]', '', ['hasLoader' => 'internal', 'historize' => false,'listenerOn'=>'body']);
+        }
+    }
+
+
+}
