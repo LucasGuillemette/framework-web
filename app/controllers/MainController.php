e89ead6a00b52ba29dc5d5d118705121db10ea37
unknown <lucas.gu7777@gmail.com>
2021-04-11 15:16:28 +0200

    finalisation du parcours du store

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -1,5 +1,6 @@
 <?php
 namespace controllers;
+use models\Basket;
 use models\Order;
 use models\Product;
 use models\Section;
@@ -29,6 +30,41 @@ class MainController extends ControllerBase{
     #[Route ('store', name:'store')]
     public function store(){
         $store = DAO::getAll(Product::class, false, false);
-        $this->loadDefaultView(['store'=>$store]);
+        $listCat = DAO::getAll(Section::class,'', ['products']);
+        $listPromo = DAO::getAll(Product::class, 'promotion< ?', false, [0]);
+        $this->loadDefaultView(['store'=>$store,'listSection'=>$listCat, 'listProm'=>$listPromo ]);
+    }
+    #[Route ('order',name:'order')]
+    public function order(){
+        $order = DAO::getAll(Order::class, 'idUser = ?', false,[USession::get("idUser")]);
+        $this->loadDefaultView(['order'=>$order]);
+    }
+
+    #[Route ('basket', name:'basket')]
+    public function basket(){
+        $basket=DAO::getAll(Basket::class, 'idUser = ?',false,[USession::get("idUser")]);
+    }
+
+    #[Route ('section/{id}', name:'section')]
+    public function section($id){
+        $product = DAO::getAll(Product::class, 'idSection= '.$id, [USession::get("idSection")]);
+        $section = DAO::getById(Section::class,$id,['products']);
+        $listsections = DAO::getAll(Section::class,'', ['products']);
+        $this->loadDefaultView(['section'=>$section, 'listSection'=>$listsections, 'product'=>$product]);
+    }
+
+    #[Route ('product/{idSection}/{idProduct}', name:'product')]
+    public function product($idSection,$idProduct){
+        $product = DAO::getAll(Product::class, 'idSection= '.$idProduct, [USession::get("idSection")]);
+        $productid = DAO::getById(Product::class,$idProduct,['sections']);
+        $section = DAO::getById(Section::class,$idSection,['products']);
+        $listsections = DAO::getAll(Section::class,'', ['products']);
+        $this->loadDefaultView(['section'=>$section, 'listSection'=>$listsections, 'product'=>$product, 'productid'=>$productid]);
+    }
+
+    #[Route('newbasket', name:'newbasket')]
+    public function newbasket(){
+
+        $user = DAO::getById(User::class, USession::get("idUser"), false);
     }
 }
